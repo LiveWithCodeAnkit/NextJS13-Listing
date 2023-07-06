d5d963a740a8c38ff60c0eee7f4a46f726c8230c
LiveWithCodeAnkit <ankityadav119live@gmail.com>
2023-07-06 23:17:53 +0530

    modify the Welcome Page

--- src/components/home/Welcome.jsx
+++ src/components/home/Welcome.jsx
@@ -7,7 +7,7 @@ const Welcome = () => {
         <div className="bg-gray-800 p-5 text-white">
           <h1 className="text-3xl font-semibold">Popular Titles</h1>
         </div>
-        <div className="flex justify-start items-start p-44 gap-6">
+        <div className="flex justify-start items-start flex-wrap p-44 gap-6">
           <div className="bg-gray-600 py-24 px-16 rounded-xl"><h1  className="text-white text-2xl font-bold ">SERIES</h1></div>
           <div className="bg-gray-600 py-24 px-16 rounded-xl"><h1 className="text-white text-2xl font-bold">Movies</h1></div>
         </div>
